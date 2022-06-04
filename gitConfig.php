c66e18a2f36256cdfd3164077432b9b850db34bf
Juaen Cruz <105850236+jmviradukunda@users.noreply.github.com>
2022-06-04 11:14:07 -0700

    nowWork
    current

---
+++ gitConfig.php
@@ -0,0 +1,28 @@
+<?php
+// Start session
+if(!session_id()){
+    session_start();
+}
+
+// Include Github client library 
+require_once 'Github_OAuth_Client.php';
+
+
+/*
+ * Configuration and setup GitHub API
+ */
+$clientID         = '01be7c6ee619844b388c';
+$clientSecret     = 'de85ee64ffdf17223386ab25d60085038138a3a8';
+$redirectURL     = 'http://localhost/special_cat/index.php';
+
+$gitClient = new Github_OAuth_Client(array(
+    'client_id' => $clientID,
+    'client_secret' => $clientSecret,
+    'redirect_uri' => $redirectURL,
+));
+
+
+// Try to get the access token
+if(isset($_SESSION['access_token'])){
+    $accessToken = $_SESSION['access_token'];
+}
